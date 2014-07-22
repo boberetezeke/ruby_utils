df326fb9ac2b0ac7e1fd70d97065cdbbc7979ba0
Steve Tuckner <steve@open-orgsolutions.com>
2014-07-22 16:38:09 -0500

    fix it so that all hidden files/directories are found

--- ff.rb
+++ ff.rb
@@ -16,7 +16,23 @@ if ARGV.size > 0 then
   end
 end
 
-(Dir["#{directory}/*"]+Dir["#{directory}/*/**/*"]).each do |fname|
+def get_fnames(directory)
+  fnames = []
+  local_fnames = Dir["#{directory}/{*,.*}"]
+  local_fnames.each do |local_fname|
+    if (File.basename(local_fname) != '.' && File.basename(local_fname) != '..')
+      if File.directory?(local_fname)
+        fnames += get_fnames(local_fname)
+      else
+        fnames.push(local_fname)
+      end
+    end
+  end
+
+  fnames
+end
+
+get_fnames(directory).each do |fname|
   if /#{file_pattern}/.match(fname) then
     puts fname
   end
